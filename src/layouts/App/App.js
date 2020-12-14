73d07d7f37c482b7b61e5dd5d4ae864c43ace0e7
agrygorkiewicz <aneta.grygorkiewicz@gmail.com>
2020-12-14 23:32:25 +0100

    contact form

--- src/layouts/App/App.js
+++ src/layouts/App/App.js
@@ -5,8 +5,8 @@ import Home from "../../components/Home/Home";
 import {BrowserRouter as Router} from "react-router-dom";
 // noinspection ES6CheckImport
 import {Route, Switch} from "react-router-dom";
-import Register from "../../components/Register/Register";
-import LoginPage2 from "../../components/Login/LoginPage2";
+// import Register from "../../components/Register/Register";
+// import LoginPage2 from "../../components/Login/LoginPage2";
 // import Logout from "../../components/Logout/Logout";
 
 
@@ -19,8 +19,8 @@ function App() {
             <Router>
                 <Switch>
                     <Route exact path="/home" component={Home}/>
-                    <Route path="/login" component={LoginPage2}/>
-                    <Route path="/register" component={Register}/>
+                    {/*<Route path="/login" component={LoginPage2}/>*/}
+                    {/*<Route path="/register" component={Register}/>*/}
                     {/*<Route path="/logout" component={Logout}/>*/}
                 </Switch>
 
