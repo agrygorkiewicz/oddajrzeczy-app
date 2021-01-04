36f165c496cf9d8dba7ca4a80c73e6a5fc456e50
agrygorkiewicz <aneta.grygorkiewicz@gmail.com>
2021-01-04 19:11:50 +0100

    exact path

--- src/layouts/App/App.js
+++ src/layouts/App/App.js
@@ -1,12 +1,11 @@
+import React from "react";
 import './App.scss';
 import Home from "../../components/Home/Home";
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-// import Nav from "../../components/Home/Nav";
-import React from "react";
 // import Register from "../../components/Register/Register";
 import Login2 from "../../components/Login/Login2";
-
+// noinspection ES6CheckImport
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
     return (
@@ -14,7 +13,7 @@ function App() {
             {/*<Home/>*/}
             <Router>
                 <Switch>
-                    <Route exact path="/home" component={Home}/>
+                    <Route  path="/home" exact component={Home}/>
                     <Route path="/login" component={Login2}/>
                     {/*<Route path="/register" component={Register}/>*/}
                 </Switch>
