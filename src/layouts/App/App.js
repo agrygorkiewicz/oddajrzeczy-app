b6fbfa58a5f5851f6425b4e015c598389314e812
agrygorkiewicz <aneta.grygorkiewicz@gmail.com>
2021-01-04 15:08:19 +0100

    routing

--- src/layouts/App/App.js
+++ src/layouts/App/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import Home from "../../components/Home/Home";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Nav from "../../components/Home/Nav";
+// import Nav from "../../components/Home/Nav";
 import React from "react";
 // import Register from "../../components/Register/Register";
 import Login2 from "../../components/Login/Login2";
@@ -10,12 +10,11 @@ import Login2 from "../../components/Login/Login2";
 function App() {
     return (
         <>
-            <Home/>
+            {/*<Home/>*/}
             <Router>
-                <Nav/>
                 <Switch>
                     <Route exact path="/home" component={Home}/>
-<Route path="/login" component={Login2}/>
+                    <Route path="/login" component={Login2}/>
                     {/*<Route path="/register" component={Register}/>*/}
                 </Switch>
 
