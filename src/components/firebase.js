3ca2df8e767f05ea59d62d1d246a499ffa0c8a79
agrygorkiewicz <aneta.grygorkiewicz@gmail.com>
2021-01-04 19:03:18 +0100

    db

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -41,16 +41,15 @@ import firebase from "firebase";
 
 
 const firebaseApp = firebase.initializeApp( {
-    apiKey: "AIzaSyBvrydp60aki3T7Jdhf14wfkB7DnR3dgYA",
-    authDomain: "hop-login.firebaseapp.com",
-    databaseURL: "https://hop-login.firebaseio.com",
-    projectId: "hop-login",
-    storageBucket: "hop-login.appspot.com",
-    messagingSenderId: "522183277390",
-    appId: "1:522183277390:web:d6278e6931616eb16c3c1f"
+    apiKey: "AIzaSyDgru-lgPinEiZanu47ScG_uVEqu_LGGTk",
+    authDomain: "nowy-41dd1.firebaseapp.com",
+    projectId: "nowy-41dd1",
+    storageBucket: "nowy-41dd1.appspot.com",
+    messagingSenderId: "265517409910",
+    appId: "1:265517409910:web:31b27cbe17dbc5e10c6157"
 });
 
 
-const db = firebaseApp//.firestore();
+export const db = firebaseApp//.firestore();
 
-export default db;
+ // default db;
